a4c654c80e37641e11a09ca1de6774d9c3ac37bf
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-05 14:09:50 +0100

    white pawns can move twice on first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,9 +10,12 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         var position = board.findPiece(this)
         var moves = []
-        if (this.player == Player.WHITE && position.row < 7){
+        if (this.player === Player.WHITE && position.row < 7){
             moves.push(Square.at(position.row+1, position.col))
-        } else if (this.player == Player.BLACK && position.row > 0){
+            if(position.row === 1){
+                moves.push(Square.at(position.row+2, position.col))
+            }
+        } else if (this.player === Player.BLACK && position.row > 0){
             moves.push(Square.at(position.row-1, position.col))
         }
         return moves;
