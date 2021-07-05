24c7e4c7c1c048b284e699134b0bf974c84aece1
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-05 14:38:21 +0100

    Bishops can move diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,27 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        var position = board.findPiece(this)
+        var moves = []
+        for(let j = -7; j < 8; j++){
+            if ((j === 0)
+                || position.col + j < 0 || position.col + j > 7
+                || position.row + j < 0 || position.row + j > 7){
+
+                continue
+            }
+            moves.push(Square.at(position.row+j,position.col+j))
+        }
+        for(let j = -7; j < 8; j++){
+            if ((j === 0)
+                || position.col + j < 0 || position.col + j > 7
+                || position.row - j < 0 || position.row - j > 7){
+
+                continue
+            }
+            moves.push(Square.at(position.row-j,position.col+j))
+        }
+
+        return moves
     }
 }
