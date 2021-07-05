a96fad42a012c50f3e2f7ae0283f982d38449762
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-05 15:39:52 +0100

    Pawns cannot jump over

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,14 +10,18 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         var position = board.findPiece(this)
         var moves = []
-        if (this.player === Player.WHITE && position.row < 7){
+        if (this.player === Player.WHITE && position.row < 7 &&
+            board.getPiece(Square.at(position.row+1, position.col)) === undefined){
             moves.push(Square.at(position.row+1, position.col))
-            if(position.row === 1){
+            if(position.row === 1 &&
+                board.getPiece(Square.at(position.row+2, position.col)) === undefined){
                 moves.push(Square.at(position.row+2, position.col))
             }
-        } else if (this.player === Player.BLACK && position.row > 0){
+        } else if (this.player === Player.BLACK && position.row > 0 &&
+            board.getPiece(Square.at(position.row-1, position.col)) === undefined){
             moves.push(Square.at(position.row-1, position.col))
-            if (position.row === 6){
+            if (position.row === 6 &&
+                board.getPiece(Square.at(position.row-2, position.col)) === undefined){
                 moves.push((Square.at(position.row-2,position.col)))
             }
         }
