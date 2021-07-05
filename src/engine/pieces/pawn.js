dc7ddb02fdd34c677d7efdabe2c8e4703bcaa4bd
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-05 14:01:52 +0100

    Allow black pawns to move forward

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,6 +12,8 @@ export default class Pawn extends Piece {
         var moves = []
         if (this.player == Player.WHITE && position.row < 7){
             moves.push(Square.at(position.row+1, position.col))
+        } else if (this.player == Player.BLACK && position.row > 0){
+            moves.push(Square.at(position.row-1, position.col))
         }
         return moves;
     }
