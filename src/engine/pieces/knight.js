06150eb5b74f66bfc22ca8e61d78b6cf0702fe94
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-05 15:05:37 +0100

    Implemented Knight Movements

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,34 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        var position = board.findPiece(this)
+        var moves = []
+        var lmoves =
+            [
+                [2,1],[1,2],
+                [-2,1],[1,-2],
+                [-1,2],[2,-1],
+                [-2,-1],[-1,-2]
+            ]
+
+        for(var i = 0; i < lmoves.length; i++){
+            var xdisplacemant = lmoves[i][0]
+            var ydisplacement = lmoves[i][1]
+
+            if(position.row + xdisplacemant < 0 ||
+                position.row + xdisplacemant > 7 ||
+                position.col + ydisplacement < 0 ||
+                position.col + ydisplacement > 7){
+                continue
+            }
+
+            moves.push(Square.at(
+                position.row+xdisplacemant,
+                position.col+ydisplacement))
+
+        }
+
+        return moves
+
     }
 }
