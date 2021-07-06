2edc0471adace52fc56a92b08b0e7ce4ef97c15f
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-06 12:32:32 +0100

    pawns can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Square from "../square";
 import Player from "../player";
+import King from "./king";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -13,13 +14,38 @@ export default class Pawn extends Piece {
         if (this.player === Player.WHITE && position.row < 7 &&
             board.getPiece(Square.at(position.row+1, position.col)) === undefined){
             moves.push(Square.at(position.row+1, position.col))
+
+            for(let x = -1; x < 2; x=x+2){
+                if(!board.validIndex(position.row+1,position.col+x)){
+                    continue
+                }
+                const opposing = board.getPiece(Square.at(position.row+1,position.col+x))
+                if (opposing !== undefined &&
+                    (opposing.player !== this.player &&
+                        !(opposing instanceof King))){
+
+                    moves.push(Square.at(position.row+1,position.col+x))
+                }
+            }
             if(position.row === 1 &&
-                board.getPiece(Square.at(position.row+2, position.col)) === undefined){
-                moves.push(Square.at(position.row+2, position.col))
+                board.getPiece(Square.at(position.row+2, position.col)) === undefined) {
+                moves.push(Square.at(position.row + 2, position.col))
             }
         } else if (this.player === Player.BLACK && position.row > 0 &&
             board.getPiece(Square.at(position.row-1, position.col)) === undefined){
             moves.push(Square.at(position.row-1, position.col))
+            for(let x = -1; x < 2; x=x+2){
+                if(!board.validIndex(position.row-1,position.col+x)){
+                    continue
+                }
+                const opposing = board.getPiece(Square.at(position.row-1,position.col+x))
+                if (opposing !== undefined &&
+                    (opposing.player !== this.player &&
+                        !(opposing instanceof King))){
+
+                    moves.push(Square.at(position.row-1,position.col+x))
+                }
+            }
             if (position.row === 6 &&
                 board.getPiece(Square.at(position.row-2, position.col)) === undefined){
                 moves.push((Square.at(position.row-2,position.col)))
